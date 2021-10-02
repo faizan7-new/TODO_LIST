1520ed56d1640a8f06dc5bd8638595b185c0409a
faizanamzad <37155071+faizanamzad@users.noreply.github.com>
2021-10-02 10:57:49 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -21,11 +21,13 @@ const Todo = ({text,todo, todos,setTodos}) => {
         <div className="todo">
             <li className={`todo-item ${todo.completed ?  "completed" : ""}`}>{text}</li>
             <button onClick = {completeHandler} className='complete-btn'>
-                <i className='fas fa-check'></i>
+                <i className='fas fa-check'>
+                </i>
             </button>
 
             <button onClick = {deleteHandler} className='trash-btn'>
-                <i className='fas fa-trash'></i>
+                <i className='fas fa-trash'>
+                </i>
             </button>
 
         </div>
